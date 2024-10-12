8a415ea94142d61412251c26166010fc5aa67792
Dreamlone <mik_sar@mail.ru>
2024-10-12 22:26:08 +0300

    Detailed exploration script added

--- atowpy/explore.py
+++ atowpy/explore.py
@@ -20,6 +20,9 @@ class DataExplorer:
         self.working_directory = working_directory.resolve()
         self.results_folder = results_folder("exploration_plots")
 
+        self.results_folder_details = Path(self.results_folder, "details")
+        self.results_folder_details.mkdir(exist_ok=True, parents=True)
+
     def show_flight_list(self):
         """
         Explore dataset through visualizations
@@ -43,11 +46,20 @@ class DataExplorer:
             - flight_duration: flight duration, min
             - taxiout_time: taxiout_time, min
             - flown_distance: route length, nmi
-            - tow: TakeOff Weight: tow [kg]
+            - tow: TakeOff Weight: tow [kg], target
         """
         df = read_challenge_set(self.working_directory)
 
-        # Check dependencies with target
+        callsigns = list(df["callsign"].unique())
+        callsigns.sort()
+        logger.info(f"Flight list exploration. Number of 'unique callsign': {len(callsigns)}")
+
+        aircraft_types = list(df["aircraft_type"].unique())
+        aircraft_types.sort()
+        logger.info(f"Flight list exploration. Number of unique 'aircraft_type': {len(aircraft_types)}")
+
+        # Details
+        self._aircraft_type_tow_plot(df, aircraft_types)
 
         self._kde_plot(df, "date", "airline", "flight_list", "rainbow")
         self._kde_plot(df, "date", "aircraft_type", "flight_list", "coolwarm")
@@ -59,18 +71,43 @@ class DataExplorer:
         challenge_df = read_challenge_set(self.working_directory)
 
         # First step - checking flight_ids overlapping
-        submission_flight_ids = set(submission_df["flight_id"].unique())
-        challenge_flight_ids = set(challenge_df["flight_id"].unique())
-        common_ids = submission_flight_ids.intersection(challenge_flight_ids)
-        if len(common_ids) < 1:
-            logger.info(f"Flight indices overlapping. No overlapping detected.")
-        else:
-            logger.info(f"Flight indices overlapping. Overlapping detected."
-                        f"Common indices {common_ids}")
+        for column_to_check in ["flight_id", "callsign"]:
+            submission_flight_ids = set(submission_df[column_to_check].unique())
+            challenge_flight_ids = set(challenge_df[column_to_check].unique())
+            common_ids = submission_flight_ids.intersection(challenge_flight_ids)
+            if len(common_ids) < 1:
+                logger.info(f"Overlapping checking. No overlapping detected for {column_to_check}.")
+            else:
+                logger.info(f"Overlapping checking. Overlapping detected for {column_to_check}."
+                            f"Common indices {len(common_ids)}")
 
         self._kde_plot(submission_df, "date", "airline", "submission_set", "rainbow")
         self._kde_plot(submission_df, "date", "aircraft_type", "submission_set", "coolwarm")
         self._kde_plot(submission_df, "date", "wtc", "submission_set", "Spectral")
+
+    def _aircraft_type_tow_plot(self, df: pd.DataFrame, aircraft_types):
+        min_tow = min(df["tow"])
+        max_tow = max(df["tow"])
+        min_flight_duration = min(df["flight_duration"])
+        max_flight_duration = max(df["flight_duration"])
+        for aircraft_type in aircraft_types:
+            logger.debug(f"Generating aircraft type vs tow plot for {aircraft_type}")
+            df_aircraft_type = df[df["aircraft_type"] == aircraft_type]
+
+            with sns.axes_style("darkgrid"):
+                fig_size = (11.0, 4.0)
+                fig, ax = plt.subplots(figsize=fig_size)
+                title = f"Aircraft type: {aircraft_type}"
+                ax = sns.stripplot(
+                    data=df_aircraft_type, x="callsign", y="tow", hue="flight_duration",
+                    hue_norm=(min_flight_duration, max_flight_duration),
+                    palette="Reds", ax=ax)
+                ax.set_ylim([min_tow, max_tow])
+                ax.set(xticklabels=[])
+                ax.set_title(title)
+                plt.savefig(Path(self.results_folder_details, f'investigation_{aircraft_type}.png'),
+                            dpi=300, bbox_inches='tight')
+                plt.close()
 
     def _kde_plot(self, df: pd.DataFrame, x_col: str, y_col: str, label: str, palette: str = "rainbow"):
         """
--- examples/explore_data.py
+++ examples/explore_data.py
@@ -11,8 +11,8 @@ def explore_dataset():
     """ Script for initial dataset scanning """
     explorer = DataExplorer(get_data_path())
 
-    explorer.show_submission_set()
     explorer.show_flight_list()
+    explorer.show_submission_set()
 
 
 if __name__ == '__main__':
